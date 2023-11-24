8d4283c8b5f56a5313aed0d28c4c771724d084c7
echeverri58 <85091279+echeverri58@users.noreply.github.com>
2023-11-24 11:57:22 -0500

    Update seguridadsanluis.py

--- seguridadsanluis.py
+++ seguridadsanluis.py
@@ -1,44 +1,33 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
-from vega_datasets import data
 
-# Lista de diccionarios con la información de cada gráfico
-info_graficos = [
-    {'url': "https://www.datos.gov.co/resource/meew-mguv.csv?$query=SELECT%0A%20%20%60departamento%60%2C%0A%20%20%60municipio%60%2C%0A%20%20%60codigo_dane%60%2C%0A%20%20%60armas_medios%60%2C%0A%20%20%60fecha_hecho%60%2C%0A%20%20%60genero%60%2C%0A%20%20%60grupo_etario%60%2C%0A%20%20%60cantidad%60%0AWHERE%0A%20%20caseless_one_of(%60departamento%60%2C%20%22ANTIOQUIA%22)%0A%20%20AND%20caseless_one_of(%60municipio%60%2C%20%22SAN%20LUIS%22)", 'titulo': 'Amenazas en San Luis Antioquia por año', 'etiqueta_x': 'Año', 'etiqueta_y': 'Cantidad'},
-    {'url': "https://www.datos.gov.co/resource/bz43-8ahq.csv?$query=SELECT%0A%20%20%60fecha_hecho%60%2C%0A%20%20%60cod_depto%60%2C%0A%20%20%60departamento%60%2C%0A%20%20%60cod_muni%60%2C%0A%20%20%60municipio%60%2C%0A%20%20%60zona%60%2C%0A%20%20%60sexo%60%2C%0A%20%20%60cantidad%60%0AWHERE%0A%20%20caseless_one_of(%60departamento%60%2C%20%22ANTIOQUIA%22)%0A%20%20AND%20caseless_one_of(%60municipio%60%2C%20%22SAN%20LUIS%22)%0AORDER%20BY%20%60fecha_hecho%60%20DESC%20NULL%20LAST", 'titulo': 'Delitos sexuales en San Luis Antioquia por año', 'etiqueta_x': 'Año', 'etiqueta_y': 'Cantidad'},
-    # ... (repite el patrón para otros gráficos)
-]
+# URL con los datos
+url = "https://www.datos.gov.co/resource/meew-mguv.csv?$query=SELECT%0A%20%20%60departamento%60%2C%0A%20%20%60municipio%60%2C%0A%20%20%60codigo_dane%60%2C%0A%20%20%60armas_medios%60%2C%0A%20%20%60fecha_hecho%60%2C%0A%20%20%60genero%60%2C%0A%20%20%60grupo_etario%60%2C%0A%20%20%60cantidad%60%0AWHERE%0A%20%20caseless_one_of(%60departamento%60%2C%20%22ANTIOQUIA%22)%0A%20%20AND%20caseless_one_of(%60municipio%60%2C%20%22SAN%20LUIS%22)"
 
-# Configuración de la página de Streamlit
-st.title("SISTEMA DE INFORMACIÓN Y SEGURIDAD CIUDADANA DEL MUNICIPIO DE SAN LUIS ANTIOQUIA")
+# Título y descripción de la aplicación
+st.title("Visualización de Datos - San Luis Antioquia")
+st.write("A continuación se presenta un gráfico interactivo sobre amenazas en San Luis Antioquia por año.")
 
-# Dividir la pantalla en dos columnas
-col1, col2 = st.beta_columns(2)
+# Cargar datos desde la URL
+df = pd.read_csv(url)
+df['fecha_hecho'] = pd.to_datetime(df['fecha_hecho'])
+df['año'] = df['fecha_hecho'].dt.year
 
-# Añadir gráficos uno por uno a cada columna
-for i, info in enumerate(info_graficos):
-    df = pd.read_csv(info['url'])
-    df['fecha_hecho'] = pd.to_datetime(df['fecha_hecho'])
-    df['fecha_hecho'] = df['fecha_hecho'].dt.year
-    cantidad_por_año = df.groupby('fecha_hecho')['cantidad'].sum().reset_index()
+# Crear gráfico interactivo con Altair
+chart = alt.Chart(df).mark_bar().encode(
+    x='año:O',
+    y='sum(cantidad):Q',
+    color='genero:N',
+    tooltip=['año:O', 'sum(cantidad):Q']
+).properties(
+    width=600,
+    title='Amenazas en San Luis Antioquia por año'
+)
 
-    # Crear el gráfico Altair con animación
-    chart = alt.Chart(cantidad_por_año).mark_bar().encode(
-        x=alt.X('fecha_hecho:O', title=info['etiqueta_x']),
-        y=alt.Y('cantidad:Q', title=info['etiqueta_y']),
-        tooltip=['fecha_hecho:N', alt.Tooltip('cantidad:Q', title='Cantidad')]
-    ).properties(
-        title=info['titulo']
-    )
+# Mostrar gráfico en Streamlit
+st.altair_chart(chart, use_container_width=True)
 
-    # Mostrar el gráfico Altair en Streamlit con animación
-    if i % 2 == 0:
-        with col1:
-            st.altair_chart(chart, use_container_width=True)
-    else:
-        with col2:
-            st.altair_chart(chart, use_container_width=True)
 
 
 
