5a537266d6f32aced214590f5147a4dfee12c8e5
echeverri58 <85091279+echeverri58@users.noreply.github.com>
2023-11-24 11:48:54 -0500

    Update seguridadsanluis.py

--- seguridadsanluis.py
+++ seguridadsanluis.py
@@ -1,6 +1,6 @@
 import streamlit as st
-import matplotlib.pyplot as plt
 import pandas as pd
+import altair as alt
 
 # Lista de diccionarios con la información de cada gráfico
 info_graficos = [
@@ -13,24 +13,19 @@ def crear_grafico(url, titulo, etiqueta_x, etiqueta_y):
     df = pd.read_csv(url)
     df['fecha_hecho'] = pd.to_datetime(df['fecha_hecho'])
     df['fecha_hecho'] = df['fecha_hecho'].dt.year
-    cantidad_por_año = df['cantidad'].groupby(df['fecha_hecho']).sum()
-    cantidad_por_año = cantidad_por_año.astype('int')
-    cantidad_por_año.index = cantidad_por_año.index.astype('str')
+    cantidad_por_año = df.groupby('fecha_hecho')['cantidad'].sum().reset_index()
 
-    fig, ax = plt.subplots()
-    ax.bar(cantidad_por_año.index, cantidad_por_año.values)
-    ax.set_xlabel(etiqueta_x)
-    ax.set_ylabel(etiqueta_y)
-    ax.set_title(titulo)
+    # Crear el gráfico Altair
+    chart = alt.Chart(cantidad_por_año).mark_bar().encode(
+        x=alt.X('fecha_hecho:O', title=etiqueta_x),
+        y=alt.Y('cantidad:Q', title=etiqueta_y),
+        tooltip=['fecha_hecho:N', alt.Tooltip('cantidad:Q', title='Cantidad')]
+    ).properties(
+        title=titulo
+    )
 
-    for i in range(len(cantidad_por_año)):
-        ax.annotate(cantidad_por_año.values[i], (cantidad_por_año.index[i], cantidad_por_año.values[i]), ha='center', va='bottom')
-
-    ax.set_xticks(cantidad_por_año.index)
-    ax.set_xticklabels(cantidad_por_año.index, rotation=90)
-
-    st.write(f"## {titulo}")
-    st.pyplot(fig)
+    # Mostrar el gráfico Altair en Streamlit
+    st.altair_chart(chart, use_container_width=True)
 
 # Configuración de la página de Streamlit
 st.title("SISTEMA DE INFORMACIÓN Y SEGURIDAD CIUDADANA DEL MUNICIPIO DE SAN LUIS ANTIOQUIA")
@@ -38,5 +33,6 @@ st.title("SISTEMA DE INFORMACIÓN Y SEGURIDAD CIUDADANA DEL MUNICIPIO DE SAN LUI
 # Añadir gráficos uno por uno
 for info in info_graficos:
     crear_grafico(info['url'], info['titulo'], info['etiqueta_x'], info['etiqueta_y'])
+
 
 
